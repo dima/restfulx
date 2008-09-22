97313b02bb289db64ae518c6a3394f21fa3fc11e
Dima Berastau <dima@ruboss.com>
2008-09-22 15:18:12 -0700

    Bumped the version to 1.0.2

--- lib/ruboss4ruby/version.rb
+++ lib/ruboss4ruby/version.rb
@@ -1,10 +1,10 @@
 module Ruboss
-  RUBOSS_FRAMEWORK_VERSION = 'latest-dima'
+  RUBOSS_FRAMEWORK_VERSION = '1.0.2'
 
   module VERSION
     MAJOR = 1
     MINOR = 0
-    TINY  = 1
+    TINY  = 2
 
     STRING = [MAJOR, MINOR, TINY].join('.')
   end
