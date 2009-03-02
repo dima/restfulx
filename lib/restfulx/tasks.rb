af506531698d171930b5961b32496c233e0a6fa2
Dima Berastau <dima.berastau@gmail.com>
2009-03-01 16:21:26 -0800

    Tweaking the tasks for compiling Flex/AIR apps a bit to make them more customlizable.

--- lib/restfulx/tasks.rb
+++ lib/restfulx/tasks.rb
@@ -7,43 +7,64 @@ require File.join(File.dirname(__FILE__), 'configuration')
 APP_ROOT = RestfulX::Configuration::APP_ROOT
 
 namespace :rx do
-  # Compile the main Flex/AIR application using given executable to the given
-  # destination folder
-  def compile_app(executable, destination, opts = '')
-    app_properties = REXML::Document.new(File.open(File.join(APP_ROOT, ".actionScriptProperties")))
-    app_properties.elements.each("*/applications/application") do |elm|
-      app_path = elm.attributes['path']
-      project_path = File.join(APP_ROOT, "app/flex", app_path)
-      target_project_path = project_path.sub(/.mxml$/, '.swf')
-      target_project_air_descriptor = project_path.sub(/.mxml$/, '-app.xml')
+  # Compile the given Flex/AIR application
+  # The following options are supported:
+  #     :executable => 'mxmlc'
+  #     :application => nil
+  #     :destination => 'public/bin'
+  #     :opts => nil
+  #     :flex_root => 'app/flex'
+  def compile_application(params = {})
+    executable = params[:executable] || 'mxmlc'
+    application = params[:application] || get_main_application
+    destination = params[:destination] || 'public/bin'
+    opts = params[:opts] || ''
+    flex_root = params[:flex_root] || 'app/flex'
+    
+    compiler = get_executable(executable)
+    
+    application = get_main_application unless application
+    project_path = File.join(APP_ROOT, flex_root, application)
+    target_project_path = project_path.sub(/.mxml$/, '.swf')
+    target_project_air_descriptor = project_path.sub(/.mxml$/, '-app.xml')
       
-      libs = Dir.glob(File.join(APP_ROOT, 'lib', '*.swc')).map {|lib| lib.gsub(' ', '\ ')}
+    libs = Dir.glob(File.join(APP_ROOT, 'lib', '*.swc')).map {|lib| lib.gsub(' ', '\ ')}
       
-      cmd = "#{executable} #{opts} -library-path+=#{libs.join(',')} " << 
-        "-keep-as3-metadata+=Resource,HasOne,HasMany,BelongsTo,DateTime,Lazy,Ignored #{project_path.gsub(' ', '\ ')}"
-      puts "Compiling #{project_path}"
-      if system(cmd)
-        FileUtils.makedirs File.join(APP_ROOT, destination)
-        puts "Moving #{target_project_path} to " + File.join(APP_ROOT, destination)
-        FileUtils.mv target_project_path, File.join(APP_ROOT, destination), :force => true
-        if File.exist?(target_project_air_descriptor)
-          descriptor = File.read(target_project_air_descriptor)
-          descriptor_name = target_project_air_descriptor.split("/").last
-          app_swf = target_project_path.split("/").last
-          descriptor.gsub!("[This value will be overwritten by Flex Builder in the output app.xml]", 
-            app_swf)
+    cmd = "#{compiler} #{opts} -library-path+=#{libs.join(',')} " << 
+      "-keep-as3-metadata+=Resource,HasOne,HasMany,BelongsTo,DateTime,Lazy,Ignored #{project_path.gsub(' ', '\ ')}"
+    puts "Compiling #{project_path}"
+    if system(cmd)
+      FileUtils.makedirs File.join(APP_ROOT, destination)
+      puts "Moving #{target_project_path} to " + File.join(APP_ROOT, destination)
+      FileUtils.mv target_project_path, File.join(APP_ROOT, destination), :force => true
+      if File.exist?(target_project_air_descriptor)
+        descriptor = File.read(target_project_air_descriptor)
+        descriptor_name = target_project_air_descriptor.split("/").last
+        app_swf = target_project_path.split("/").last
+        descriptor.gsub!("[This value will be overwritten by Flex Builder in the output app.xml]", 
+          app_swf)
 
-          File.open("#{APP_ROOT}/#{destination}/#{descriptor_name}", "w") do |file|
-            file.print descriptor
-          end
-          puts "Created #{APP_ROOT}/#{destination}/#{descriptor_name} descriptor."
+        File.open("#{APP_ROOT}/#{destination}/#{descriptor_name}", "w") do |file|
+          file.print descriptor
         end
-        puts 'Done!'
-      else
-        puts "The application was not compiled. Check console for errors. " <<
-          "It is possible that '(a)mxmlc' executable was not found or there are compilation errors."
+        puts "Created #{APP_ROOT}/#{destination}/#{descriptor_name} descriptor."
       end
-    end    
+      puts 'Done!'
+    else
+      puts "The application was not compiled. Check console for errors. " <<
+        "It is possible that '(a)mxmlc' executable was not found or there are compilation errors."
+    end
+  end
+  
+  def run_air_application(params = {})
+    application = params[:application] || get_main_application
+    descriptor = params[:descriptor] || application.sub(/.mxml$/, '-app.xml')
+    destination = params[:destination] || 'bin-debug'
+    
+    puts "Running AIR application with descriptor: #{descriptor}"
+    if !system("#{get_executable('adl')} #{destination}/#{descriptor}")
+      puts "Could not run the application with descriptor: #{destination}/#{descriptor}. Check console for errors."
+    end
   end
   
   # Find what the main application is based on .actionScriptProperties file
@@ -63,23 +84,19 @@ namespace :rx do
   namespace :flex do
     desc "Build project swf file and move it into public/bin folder"
     task :build do
-      compile_app(get_executable('mxmlc'), 'public/bin')
+      compile_application()
     end
   end
   
   namespace :air do
     desc "Build project swf file as an AIR application and move it into bin-debug folder"
     task :build do   
-      compile_app(get_executable('mxmlc'), 'bin-debug', '+configname=air')
+      compile_application(:destination => 'bin-debug', :opts => '+configname=air')
     end
     
     desc "Run the AIR application (if this project is configured as an AIR project)"
     task :run do
-      target = get_main_application.gsub(/.mxml$/, '-app.xml')
-      puts "Running AIR application with descriptor: #{target}"
-      if !system("#{get_executable('adl')} bin-debug/#{target}")
-        puts "Could not run the application with descriptor: #{target}. Check console for errors."
-      end
+      run_air_application
     end
   end
 end
--- rails_generators/rx_config/templates/restfulx_tasks.rake
+++ rails_generators/rx_config/templates/restfulx_tasks.rake
@@ -3,4 +3,7 @@ begin
   # this will load the latest restfulx gem version
   require 'restfulx/active_record_tasks'
 rescue LoadError
+  puts "Could not load active_record_tasks from 'restfulx/active_record_tasks'. " <<
+    "If you are loading restfulx gem from vendor/gems, adjust the path to active_record_tasks in " <<
+    "lib/tasks/restfulx_tasks.rake accordingly."
 end
